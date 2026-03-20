cab11db04f4675b7c5ca7aa2290a4030b17e235a
PoistJ <157406020+PoistJ@users.noreply.github.com>
2026-03-20 01:38:49 -0400

    Added post to the /sign-up route.

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const crypto = require("crypto");
+const { name } = require("ejs");
 
 require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  port: process.env.PORT,
+  database: process.env.DB,
+});
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -29,7 +38,22 @@ app.use(
 app.get("/", (req, res) => res.render("home"));
 
 app.get("/sign-up", (req, res) => res.render("sign-up"));
-
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    await pool.query(
+      "INSERT INTO members (first_name, last_name, username, password, status) VALUES ($1, $2, $3, $4, $5)",
+      [
+        req.body.first - name,
+        req.body.last - name,
+        req.body.username,
+        req.body.password,
+        "anon",
+      ],
+    );
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.listen(3000, (error) => {
   if (error) {
