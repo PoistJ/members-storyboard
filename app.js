abf4d3f0555a9a8f520e9796f42216dde1d1c107
PoistJ <157406020+PoistJ@users.noreply.github.com>
2026-03-22 02:10:13 -0400

    Refactored db connection details to .env, added initial form validation to firstName field of sign-up page.

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
-const { name } = require("ejs");
+const { body, validationResult } = require("express-validator");
 
 require("dotenv").config();
 
 const pool = new Pool({
-  host: "localhost",
-  user: "poistj",
-  password: "0Omgiwasbaned",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
   port: 5432,
-  database: "storyboard",
+  database: process.env.DB,
 });
 
 const app = express();
@@ -63,25 +63,35 @@ passport.use(
 app.get("/", (req, res) => res.render("home"));
 
 app.get("/sign-up", (req, res) => res.render("sign-up"));
-app.post("/sign-up", async (req, res, next) => {
-  try {
-    const hashedPass = await bcrypt.hash(req.body.password, 10);
-    await pool.query(
-      "INSERT INTO members (first_name, last_name, username, password, status) VALUES ($1, $2, $3, $4, $5)",
-      [
-        req.body.firstName,
-        req.body.lastName,
-        req.body.username,
-        hashedPass,
-        "anon",
-      ],
-    );
+app.post(
+  "/sign-up",
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isAlpha()
+    .isLength({ min: 2, max: 30 })
+    .withMessage("Must be at least 2 or less than 30 characters"),
+  async (req, res, next) => {
+    try {
+      const hashedPass = await bcrypt.hash(req.body.password, 10);
+      await pool.query(
+        "INSERT INTO members (first_name, last_name, username, password, status) VALUES ($1, $2, $3, $4, $5)",
+        [
+          req.body.firstName,
+          req.body.lastName,
+          req.body.username,
+          hashedPass,
+          "anon",
+        ],
+      );
 
-    res.redirect("/");
-  } catch (err) {
-    return next(err);
-  }
-});
+      res.redirect("/");
+    } catch (err) {
+      return next(err);
+    }
+  },
+);
 
 app.listen(3000, (error) => {
   if (error) {
