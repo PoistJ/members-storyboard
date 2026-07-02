e6017322cfa83baf940665a1460bf8e9c06ea7da
PoistJ <157406020+PoistJ@users.noreply.github.com>
2026-07-01 20:24:03 -0400

    Modified pg-pool connection in indexController.js to use Railway's psql database.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,11 +5,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASS,
-  port: process.env.PORT,
-  database: process.env.DB,
+  host: PGHOST,
+  user: PGPUSER,
+  password: PGPASSWORD,
+  port: PGPORT,
+  database: PGDATABASE,
 });
 
 const validateUser = [
